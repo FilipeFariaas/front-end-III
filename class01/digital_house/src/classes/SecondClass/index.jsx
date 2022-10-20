b9c3ec7c7c1a48116b6374a846d38c4ae9dcd851
FilipeFariaas <filipefarias117@gmail.com>
2022-10-19 22:46:28 -0300

    add class 02

--- class01/digital_house/src/classes/SecondClass/index.jsx
+++ class01/digital_house/src/classes/SecondClass/index.jsx
@@ -4,6 +4,32 @@ export function SecondClass() {
     return (
         <main>
             <h1>Segunda Aula</h1>
+            <h2>Site - <a href={"https://www.starplus.com/en-br"} target={"_blank"} >Star+ (não logado)</a> </h2>
+            <br/>
+            <h3>COMPONENTES</h3>
+            <ul>
+                <li>
+                    <p>Header</p>
+                </li>
+                <li>
+                    <p>Banner</p>
+                </li>
+                <li>
+                    <p>Buttons</p>
+                </li>
+                <li>
+                    <p>Plans Cards</p>
+                </li>
+                <li>
+                    <p>Sections</p>
+                </li>
+                <li>
+                    <p>Accordion-Container - Perguntas Frequentes</p>
+                </li>
+                <li>
+                    <p>Footer</p>
+                </li>
+            </ul>
         </main>
     )
 }
